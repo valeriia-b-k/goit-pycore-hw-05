f1d7f0ddf3c4876d8b3ab7aa5168dbffd1e7787f
valeriia-b-k <valeria.bohdan@gmail.com>
2025-11-05 11:06:39 +0200

    Add 3 homework files

---
+++ goit-pycore-hw-05_2.py
@@ -0,0 +1,19 @@
+import re
+from typing import Callable, Generator
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+    pattern = r"\d+\.\d+"
+    for match in re.findall(pattern, text):
+        yield float(match)
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    return sum(func(text))
+
+text = (
+    "Загальний дохід працівника складається з декількох частин: "
+    "1000.01 як основний дохід, доповнений додатковими надходженнями "
+    "27.45 і 324.00 доларів."
+)
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
---
+++ goit-pycore-hw-05_4.py
@@ -0,0 +1,71 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Please specify a name."
+        except KeyError:
+            return "Contact not found."
+    return inner
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        raise KeyError
+
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+def show_all(contacts):
+    if contacts:
+        result = ""
+        for name, phone in contacts.items():
+            result += f"{name}: {phone}\n"
+        return result.strip()
+    else:
+        return "No contacts found."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
